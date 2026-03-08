435db6f48ec6c25a21a9afef340c23e25bedc82a
anabch20 <anabelcchavez10@gmail.com>
2026-03-08 00:04:36 -0500

    logoutBtn

--- frontend/src/styles/styles.js
+++ frontend/src/styles/styles.js
@@ -209,9 +209,9 @@ loginBg: {
   },
 
   logoutBtn: {
-    background: "transparent",
+    background: "#EFECE3",
     border: "1px solid #2a2a3e",
-    color: "#6b7280",
+    color: "#ffffff",
     padding: "4px 12px",
     borderRadius: 6,
     cursor: "pointer",
